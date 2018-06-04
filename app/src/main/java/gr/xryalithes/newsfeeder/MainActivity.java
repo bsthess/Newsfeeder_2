cf602ec23dc609bdd571091102d1ea29bc2c8ff8
Bsthess <bsthess@yahoo.gr>
2018-06-05 00:46:49 +0300

    Added thumbnail image,made important corrections to the code,added connectivity check and message.

--- app/src/main/java/gr/xryalithes/newsfeeder/MainActivity.java
+++ app/src/main/java/gr/xryalithes/newsfeeder/MainActivity.java
@@ -5,23 +5,28 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.net.ConnectivityManager;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Article>> {
 
 
-    public static final String LOG_TAG = MainActivity.class.getName();
-    private static final String URL_TO_JSON = "https://content.guardianapis.com/search?section=football&order-by=newest&show-elements=image&show-fields=byline%2Cthumbnail&page=1&page-size=10&api-key=test";
+
+    private static final String URL_TO_JSON = "https://content.guardianapis.com/search";
+    private static final String URL_TEST="https://content.guardianapis.com/search?section=football&order-by=newest&show-elements=image&show-fields=byline%2Cthumbnail&page=1&page-size=10&api-key=test";
     private static final int ARTICLE_LOADER_ID = 1;
     private ArticleAdapter mAdapter;
     private TextView mEmptyStateTextView;
@@ -30,16 +35,20 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         /////////////// CREATE A LOADER MANAGER AND INITIALIZE A LOADER///////////////////////////////
         LoaderManager loaderManager = getLoaderManager();
         loaderManager.initLoader(ARTICLE_LOADER_ID, null, this);
+
         //////////////DECLARE A LISTVIEW AND SETTING THE EMPTYSTATE OF IT////////////////////////
         ListView articleListView = findViewById(R.id.list);
         mEmptyStateTextView = findViewById(R.id.empty);
         articleListView.setEmptyView(mEmptyStateTextView);
+
         ////////////CREATE AN ARTICLE ADAPTER THAT HOLDS ARTICLE OBJECTS AND CONNECT IT TO LISTVIEW////////////////////
         mAdapter = new ArticleAdapter(this, new ArrayList<Article>());
         articleListView.setAdapter(mAdapter);
+
         //////////WHEN CLICK, OPEN THE ARTICLE WEBSITE////////////////////////////////////////
         articleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -62,11 +71,40 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             return false;
         }
     }
-////////////
+
     @Override
     public Loader<List<Article>> onCreateLoader(int i, Bundle bundle) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        return new ArticleLoader(this, URL_TO_JSON);
+        // getString retrieves a String value from the preferences. The second parameter is the default value for this preference.
+        String sectionSelected = sharedPrefs.getString(
+                getString(R.string.settings_SECTION_key),
+                getString(R.string.settings_section_default));
+
+
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(URL_TO_JSON);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value.
+        // This is the format of the  url we want to create
+        // "https://content.guardianapis.com/search?section=football&order-by=newest&
+        // show-elements=image&show-fields=byline%2Cthumbnail&page=1&page-size=10&api-key=test"
+
+        uriBuilder.appendQueryParameter("section",sectionSelected);
+        uriBuilder.appendQueryParameter("order-by", "newest");
+        uriBuilder.appendQueryParameter("show-elements", "image");
+        uriBuilder.appendQueryParameter("show-fields", "byline,thumbnail");
+        uriBuilder.appendQueryParameter("page","1");
+        uriBuilder.appendQueryParameter("page-size", "10");
+        uriBuilder.appendQueryParameter("api-key", "test");
+        String urlFinal = uriBuilder.toString();
+        Log.v("url is :",urlFinal);
+        return new ArticleLoader(this, urlFinal);
+
     }
     @Override
     public void onLoadFinished(Loader<List<Article>> loader, List<Article> articles) {
@@ -92,4 +130,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         mAdapter.clear();
     }
 
+    @Override
+    // This method initialize the contents of the Activity's options menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    // This method is called whenever an item in the options menu is selected.
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
